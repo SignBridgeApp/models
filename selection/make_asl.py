639133f696bb22fade513e3354cb8035171c27fc
Bipin <87369440+bipinkrish@users.noreply.github.com>
2024-04-09 23:35:10 +0530

    Add files via upload

--- selection/make_asl.py
+++ selection/make_asl.py
@@ -1,7 +1,6 @@
 import json
 import os
 import shutil
-import csv
 
 if not os.path.exists("asl"):
     os.makedirs("asl")
@@ -9,25 +8,11 @@ if not os.path.exists("asl"):
 with open("selected.json") as f:
     selected = json.load(f)
     
-csv_data = []
+words = []
 for gloss in selected:
     shutil.copy2(f"poses/{selected[gloss]}.pose", f"asl/{gloss}.pose")
-    csv_data.append({
-        "path": f"asl/{gloss}.pose",
-        "spoken_language": "en",
-        "signed_language": "asl",
-        "start": 0,
-        "end": 0,
-        "words": gloss,
-        "glosses": gloss,
-        "priority": 0
-    })
+    words.append(gloss.lower().strip())
     
-csv_filename = "index.csv"
-with open(csv_filename, mode="w", newline="") as file:
-    fieldnames = ["path", "spoken_language", "signed_language", "start", "end", "words", "glosses", "priority"]
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
-    
-    writer.writeheader()
-    for row in csv_data:
-        writer.writerow(row)
+words_file = "words.txt"
+with open(words_file, mode="w") as file:
+    file.write("\n".join(words))
